7b909b5f5d54e4c3e3b3fa8d9b60d55a2c3157a7
Jacqueline Rudolph <53434512+jerudolph13@users.noreply.github.com>
2025-10-23 15:58:04 -0400

    Create run_cif-divide.sh

---
+++ run_cif-divide.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+
+#SBATCH --job-name=sampling
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=jacqueline.rudolph@jhu.edu
+#SBATCH --output=./results/cif_option.out
+
+#SBATCH --partition=lau
+#SBATCH --nodelist=compute-135
+#SBATCH --time=7-00:00:00
+#SBATCH --mem=memory
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --verbose
+
+t=$(date)
+echo -e "Start time: $t \n"
+d=$(pwd)
+echo -e "Working directory: $d \n"
+
+echo -e "Analysis: option \n"
+
+module load R
+#script ./code/samp_2_cif-divide.R option
+Rscript ./code/samp_2_cif-divide-repeat.R option
+
+echo
+sstat -a -o JobID,MaxVMSizeNode,MaxVMSize,AveVMSize,MaxRSS,AveRSS,MaxDiskRead,MaxDiskWrite,AveCPUFreq,TRESUsageInMax -j ${SLURM_JOB_ID}
+echo
+t=$(date)
+echo "End time: $t"
